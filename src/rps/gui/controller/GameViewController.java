845188b2f61da7760d8ca3dfdc835b6b4536f783
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-02-27 14:56:32 +0100

    super basic GUI

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -1,7 +1,12 @@
 package rps.gui.controller;
 
 // Java imports
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,11 +16,42 @@ import java.util.ResourceBundle;
  */
 public class GameViewController implements Initializable {
 
+    @FXML
+    private ImageView imageGameIcon;
+
+    @FXML
+    private Label labelAIMove, labelWinner, labelYourMove;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+
+    @FXML
+    void clickMainMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickPaper(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickRestart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickRock(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickScissors(ActionEvent event) {
+
     }
 }
