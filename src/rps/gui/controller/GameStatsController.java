7e910187e432f3365e16653cff276c4793807405
Matej Mazúr <matejmazur@outlook.com>
2023-02-28 12:18:49 +0100

    Add the platform.runlater

--- src/rps/gui/controller/GameStatsController.java
+++ src/rps/gui/controller/GameStatsController.java
@@ -3,6 +3,8 @@ package rps.gui.controller;
 import io.github.palexdev.materialfx.controls.MFXTableColumn;
 import io.github.palexdev.materialfx.controls.MFXTableView;
 import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -20,10 +22,13 @@ public class GameStatsController implements Initializable {
     private GameManager gm;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        SetupTable();
+        Platform.runLater(() -> {
+            SetupTable();
+        });
     }
     public void setGameManager(GameManager gm){
         this.gm = gm;
+        System.out.println("GameStatsController.setGameManager() gm = " + gm);
     }
     private void SetupTable(){
         MFXTableColumn<Result> roundNumberColumn = new MFXTableColumn<>("RoundNumber", true, Comparator.comparing(Result::getRoundNumber));
@@ -54,8 +59,9 @@ public class GameStatsController implements Initializable {
         }));
         loserMoveColumn.setRowCellFactory(result -> new MFXTableRowCell<>(Result::getLoserMove));
 
-
         table.getTableColumns().addAll(roundNumberColumn, winnerColumn, winnerMoveColumn, loserColumn, loserMoveColumn);
-        //table.setItems((ObservableList<Result>) gm.getGameState().getHistoricResults());
+        System.out.println("GameStatsController.SetupTable() gm = " + gm);
+        if (gm != null)
+            table.setItems(FXCollections.observableArrayList(gm.getGameState().getHistoricResults())); //TODO: change to observable list in game manager
     }
 }
