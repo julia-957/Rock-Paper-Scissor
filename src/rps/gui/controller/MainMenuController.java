fd54bc546890dd1aeaacd0b92f1b7a86621427ab
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-03-01 14:26:15 +0100

    login by enter and fonts

--- src/rps/gui/controller/MainMenuController.java
+++ src/rps/gui/controller/MainMenuController.java
@@ -3,24 +3,29 @@ package rps.gui.controller;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import rps.bll.player.IPlayer;
 import rps.bll.player.Player;
 import rps.bll.player.PlayerType;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainMenuController {
+public class MainMenuController implements Initializable {
     @FXML private MFXButton btnStartGame;
     @FXML private MFXTextField txtPlayerName;
 
     @FXML
-    void btnStartAction(ActionEvent actionEvent) throws IOException {
+    void btnStartAction(Event event) throws IOException {
         if (!txtPlayerName.getText().isEmpty()){
             IPlayer human = new Player(txtPlayerName.getText().trim(), PlayerType.Human);
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/rps/gui/view/GameView.fxml"));
@@ -33,11 +38,25 @@ public class MainMenuController {
             gameViewController.setHuman(human);
             stage.show();
 
-            Node node = (Node) actionEvent.getSource();
+            Node node = (Node) event.getSource();
             node.getScene().getWindow().hide();
         }
         else {
             new Alert(Alert.AlertType.ERROR, "Player name cannot be empty!").show();
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        txtPlayerName.setOnKeyReleased(event -> {
+            if(event.getCode()== KeyCode.ENTER) {
+                try {
+                    btnStartAction(event);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        });
     }
 }
